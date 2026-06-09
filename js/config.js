5243985ce89bd44bcae2724d14e5a89daac14714
SumitRane8 <sumitrane1406@gmail.com>
2026-06-09 15:38:58 +0530

    Fix Worker URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SPAD_CONFIG = {
-  workerUrl: 'https://spad-builder-api.sumitrane1406.workers.dev',
+  workerUrl: 'https://blue-truth-ff67.sumitrane1406.workers.dev',
   owner: 'SumitRane1',
   repo: 'zmk-config-macropad',
 };
